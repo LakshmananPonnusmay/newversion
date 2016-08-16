8e2bec3049d2538bd4c3cc4edd431cce7ed19329
karthikchandransmaat <karthik.c@smaatapps.com>
2016-08-16 16:04:11 +0530

    Update create_bank_recipient_agent.php

--- stripe/create_bank_recipient_agent.php
+++ stripe/create_bank_recipient_agent.php
@@ -3,7 +3,7 @@ require(dirname(__FILE__) . '/lib/Stripe.php');
 //require 'lib/Stripe.php';
 Stripe::setApiKey("sk_test_l8FVfiAVF7kbHq3oGOD2hCkL");
 
-error_reporting(0);			 
+error_reporting(E_ALL);			 
  
   try{
 			 
@@ -74,6 +74,7 @@ error_reporting(0);
 		}
 		catch (Exception $e) {
 		$error = "false,".$e->getMessage();
+		echo $e->getMessage();
 		echo '0';
 		}
 
