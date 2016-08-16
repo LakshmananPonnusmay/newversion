1738d023279708846e63e410a0aed65963a64283
karthikchandransmaat <karthik.c@smaatapps.com>
2016-08-16 16:11:11 +0530

    Update create_bank_recipient_agent.php

--- stripe/create_bank_recipient_agent.php
+++ stripe/create_bank_recipient_agent.php
@@ -6,7 +6,15 @@ Stripe::setApiKey("sk_test_l8FVfiAVF7kbHq3oGOD2hCkL");
 error_reporting(E_ALL);			 
  
   try{
-			 
+		$customer = Stripe_Customer::create(array(
+			'email'    => "kumar@gmail.com",
+			'description' => "bank"
+		 ));
+		
+	
+		$customerId = $customer->id; 
+	       
+			
 	
 		echo '1';
 		
